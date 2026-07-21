b573cdccc0ec08afa7b79a923537d5488727bb67
minsu7805 <minsu7805@naver.com>
2026-07-21 12:49:04 +0900

    Update team hero photo order and showreel video to hM43pyC1oJY.

--- videos.js
+++ videos.js
@@ -2,7 +2,7 @@
   // YouTube 영상 ID만 넣으면 페이지에서 바로 재생됩니다.
   // 예: https://www.youtube.com/watch?v=ABC123 → id: 'ABC123'
   var videos = [
-    { id: '5Tma0V1EF9w' }
+    { id: 'hM43pyC1oJY' }
   ];
 
   var grid = document.querySelector('.video-grid');
